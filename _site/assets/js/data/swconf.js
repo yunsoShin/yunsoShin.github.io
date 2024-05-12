c47e5887722ea17d5d040f340f2ccf3543453291
Yunsu <shin4895@gmail.com>
2024-05-12 20:44:23 +0900

    edit stencil SEO

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715514170',resources: [
+    cacheName: 'chirpy-1715514237',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
