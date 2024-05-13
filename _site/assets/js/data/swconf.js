d1611828eee91fb7bc9ea82d54a7b13c7bd17bd3
Yunsu <shin4895@gmail.com>
2024-05-13 20:55:37 +0900

    필요없는 하이퍼링크 삭제

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715600654',resources: [
+    cacheName: 'chirpy-1715601301',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
