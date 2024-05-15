8163ead3a811692ce0935807dbcab8e874eadcfb
Yunsu <shin4895@gmail.com>
2024-05-15 20:13:04 +0900

    read time 코드 줄을 삭제

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715771458',resources: [
+    cacheName: 'chirpy-1715771565',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
