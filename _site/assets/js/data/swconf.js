c959a4abaa0cd66bedaff4ec1e75682495bf20cb
Yunsu <shin4895@gmail.com>
2024-05-15 19:59:20 +0900

    baseURL update

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715768786',resources: [
+    cacheName: 'chirpy-1715770726',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
