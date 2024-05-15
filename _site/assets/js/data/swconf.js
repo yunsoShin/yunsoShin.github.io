0d45ae7c0ce5ab6bd73208537454415f10b3dffe
Yunsu <shin4895@gmail.com>
2024-05-15 20:46:40 +0900

    update avatar

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715773250',resources: [
+    cacheName: 'chirpy-1715773552',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
