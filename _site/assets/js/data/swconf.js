330dcf9846bc6a0c299758aa3d43126262314882
Yunsu <shin4895@gmail.com>
2024-05-15 18:42:16 +0900

    update-yml CDN URL

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715765666',resources: [
+    cacheName: 'chirpy-1715765917',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
