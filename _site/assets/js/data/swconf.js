57ad4c097a8af47ab7107e78691aca5eb918bbce
Yunsu <shin4895@gmail.com>
2024-05-15 19:26:47 +0900

    img 그라디언트만 제거

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1715768487',resources: [
+    cacheName: 'chirpy-1715768786',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
