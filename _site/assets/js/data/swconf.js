59e03faaebafbce27582533960e0ffc5ed3021b4
Yunsu <shin4895@gmail.com>
2024-06-19 23:56:10 +0900

    post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1718711702',resources: [
+    cacheName: 'chirpy-1718808961',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
